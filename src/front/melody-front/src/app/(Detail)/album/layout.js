a3734e1d1971cd017acc13042d3eba694ac13ebd
bogye <bogyoem1@naver.com>
2023-09-27 13:58:39 +0900

    commit before pull

---
+++ src/front/melody-front/src/app/(Detail)/album/layout.js
@@ -0,0 +1,9 @@
+const DetailPage = () => {
+    return (
+        <div className="py-8">
+            <h1>아자자자자장</h1>
+        </div>
+    )
+}
+
+export default DetailPage
---
+++ src/front/melody-front/src/app/(Detail)/song/page.js
@@ -0,0 +1,13 @@
+"use client"
+
+import RegistrationForm2 from "../../../components/user/RegistrationForm2";
+
+const Page = () => {
+    return (
+        <div>
+            <h2 SoloArtists />
+            <RegistrationForm2 />
+        </div>
+    )
+}
+export default Page;
